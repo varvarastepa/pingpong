a449c5d0ae8fe604aa39fc52c037ef2c5e83238b
Varvara <147473219+varvarastepa@users.noreply.github.com>
2023-10-12 09:33:48 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -1,40 +1,38 @@
-Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from pygame import *
-... '''Необходимые классы'''
-... 
-... #класс-родитель для спрайтов
-... class GameSprite(sprite.Sprite):
-...    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
-...        super().__init__()
-...        self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
-...        self.speed = player_speed
-...        self.rect = self.image.get_rect()
-...        self.rect.x = player_x
-...        self.rect.y = player_y
-... 
-...    def reset(self):
-...        window.blit(self.image, (self.rect.x, self.rect.y))
-... 
-... class Player(GameSprite):
-...    def update_r(self):
-...        keys = key.get_pressed()
-...        if keys[K_UP] and self.rect.y > 5:
-...            self.rect.y -= self.speed
-...        if keys[K_DOWN] and self.rect.y < win_height - 80:
-...            self.rect.y += self.speed
-...    def update_l(self):
-...        keys = key.get_pressed()
-...        if keys[K_w] and self.rect.y > 5:
-...            self.rect.y -= self.speed
-...        if keys[K_s] and self.rect.y < win_height - 80:
-...            self.rect.y += self.speed
-... 
-... #игровая сцена:
-... back = (200, 255, 255) #цвет фона (background)
-... win_width = 600
-... win_height = 500
-... window = display.set_mode((win_width, win_height))
+from pygame import *
+'''Необходимые классы'''
+
+#класс-родитель для спрайтов
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+   def update_r(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+   def update_l(self):
+       keys = key.get_pressed()
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+
+#игровая сцена:
+back = (200, 255, 255) #цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
 window.fill(back)
 
 #флаги, отвечающие за состояние игры
@@ -91,6 +89,10 @@ while game:
        racket1.reset()
        racket2.reset()
        ball.reset()
+
+   display.update()
+   clock.tick(FPS)
+
 
    display.update()
    clock.tick(FPS)
